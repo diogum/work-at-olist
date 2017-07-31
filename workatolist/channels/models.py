219b496499252f343edd3dcdd864e8187fc85efb
Diogo Gonçalves <goncalveshd@gmail.com>
2017-07-30 21:45:38 -0300

    Add a base class for models

--- workatolist/channels/models.py
+++ workatolist/channels/models.py
@@ -1,3 +1,13 @@
+from uuid import uuid4
 from django.db import models
 
-# Create your models here.
+
+class BaseModel(models.Model):
+    """Abstract base class that provides primary key, creation and modification timestamp."""
+
+    id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
+    created = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
