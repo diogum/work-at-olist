ea10cc8b216091a110145294eef01f6f086c2572
Diogo Gonçalves <goncalveshd@gmail.com>
2017-07-31 20:04:11 -0300

    Add categories model

--- workatolist/channels/models.py
+++ workatolist/channels/models.py
@@ -1,5 +1,6 @@
 from uuid import uuid4
 from django.db import models
+from mptt.models import MPTTModel, TreeForeignKey
 
 
 class BaseModel(models.Model):
@@ -17,6 +18,17 @@ class Channel(BaseModel):
     """Channels model for marketplaces."""
 
     name = models.CharField('Name', max_length=30, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Category(MPTTModel, BaseModel):
+    """Hierarchical model for categories."""
+
+    name = models.CharField('Name', max_length=30)
+    channel = models.ForeignKey(Channel, related_name='categories')
+    parent = TreeForeignKey('self', related_name='children', null=True, blank=True, db_index=True)
 
     def __str__(self):
         return self.name
--- workatolist/channels/tests/test_models.py
+++ workatolist/channels/tests/test_models.py
@@ -1,10 +1,14 @@
 from django.test import TestCase
 from django.db.utils import IntegrityError
-from ..models import Channel
+from ..models import Channel, Category
 
 
 def create_channel(name):
     return Channel.objects.create(name=name)
+
+
+def create_category(name, channel, parent=None):
+    return Category.objects.create(name=name, channel=channel, parent=parent)
 
 
 class ChannelTestCase(TestCase):
@@ -18,6 +22,54 @@ class ChannelTestCase(TestCase):
     def test__name_must_be_unique(self):
         """Channel's name must be unique"""
         create_channel('Unique')
-
         with self.assertRaises(IntegrityError):
             create_channel('Unique')
+
+
+class CategoryTestCase(TestCase):
+    """Category tests."""
+
+    def test__str(self):
+        """String representation must be the category's name."""
+        channel = create_channel('Channel Sample')
+        category = create_category('String Representation', channel)
+        self.assertEqual(str(category), category.name)
+
+    def test__root_category(self):
+        """Root category has no parent."""
+        channel = create_channel('Channel Sample')
+        category = create_category('Root Category', channel, parent=None)
+        self.assertIsNone(category.parent)
+        self.assertTrue(category.is_root_node())
+
+    def test__child_category(self):
+        """Child category has parent."""
+        channel = create_channel('Channel Sample')
+        root = create_category('Root Category', channel, parent=None)
+        child = create_category('Child Category', channel, parent=root)
+        self.assertEqual(child.parent, root)
+        self.assertTrue(child.is_child_node())
+
+    def test__category_with_children(self):
+        """Category can have children."""
+        channel = create_channel('Channel Sample')
+        root = create_category('Root Category', channel, parent=None)
+        create_category('Child 1 Category', channel, parent=root)
+        create_category('Child 2 Category', channel, parent=root)
+        self.assertEqual(root.get_children().count(), 2)
+
+    def test__category_siblings(self):
+        """Category siblings."""
+        channel = create_channel('Channel Sample')
+        root = create_category('Root Category', channel, parent=None)
+        child1 = create_category('Child 1 Category', channel, parent=root)
+        child2 = create_category('Child 2 Category', channel, parent=root)
+        self.assertEqual(child1.get_next_sibling(), child2)
+        self.assertEqual(child2.get_previous_sibling(), child1)
+
+    def test__leaf_category(self):
+        """Category is a leaf."""
+        channel = create_channel('Channel Sample')
+        root = create_category('Root Category', channel, parent=None)
+        leaf = create_category('Leaf Category', channel, parent=root)
+        self.assertTrue(leaf.is_leaf_node())
