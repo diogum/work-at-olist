eb22ca85da602c268faaa0db4dfea98ded374890
Diogo Gonçalves <goncalveshd@gmail.com>
2017-08-01 20:49:25 -0300

    Set sort and insertion order

--- workatolist/channels/models.py
+++ workatolist/channels/models.py
@@ -32,6 +32,7 @@ class Channel(BaseModel):
     name = models.CharField(_('Name'), max_length=30, unique=True)
 
     class Meta:
+        ordering = ['name']
         verbose_name = _('Channel')
         verbose_name_plural = _('Channels')
 
@@ -52,6 +53,9 @@ class Category(MPTTModel, BaseModel):
     class Meta:
         verbose_name = _('Category')
         verbose_name_plural = _('Categories')
+
+    class MPTTMeta:
+        order_insertion_by = ['channel', 'name']
 
     def __str__(self):
         return self.name
