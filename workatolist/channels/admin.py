4a2662dde569549cc9ef326a0558d39c9b49f899
Diogo Gonçalves <goncalveshd@gmail.com>
2017-08-01 20:25:17 -0300

    Reference ID must be readonly

--- workatolist/channels/admin.py
+++ workatolist/channels/admin.py
@@ -7,10 +7,12 @@ from .models import Category, Channel
 
 class ChannelAdmin(admin.ModelAdmin):
     list_display = ('name', 'reference_id',)
+    readonly_fields = ('reference_id',)
 
 
 class CategoryAdmin(MPTTModelAdmin):
     list_display = ('name', 'reference_id', 'channel', 'parent',)
+    readonly_fields = ('reference_id',)
 
 
 admin.site.register(Channel, ChannelAdmin)
