f1cbd9de9b7b0509098908334d7949dfda8b6d7b
Diogo Gonçalves <goncalveshd@gmail.com>
2017-08-01 20:20:35 -0300

    Add models to Django admin

--- workatolist/channels/admin.py
+++ workatolist/channels/admin.py
@@ -2,5 +2,14 @@ from django.contrib import admin
 
 from .models import Category, Channel
 
-admin.site.register(Channel)
-admin.site.register(Category)
+
+class ChannelAdmin(admin.ModelAdmin):
+    list_display = ('name', 'reference_id',)
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'reference_id', 'channel', 'parent',)
+
+
+admin.site.register(Channel, ChannelAdmin)
+admin.site.register(Category, CategoryAdmin)
