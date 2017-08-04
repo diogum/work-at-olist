a63b1d2b9c1716c6abdfb8210ae0da95eebe222a
Diogo Gonçalves <goncalveshd@gmail.com>
2017-08-03 23:23:29 -0300

    Fix default config setting

--- workatolist/workatolist/settings.py
+++ workatolist/workatolist/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config("DEBUG", default=False, cast=config.boolean)
 
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', default=[], cast=config.list)
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', default=['*'], cast=config.list)
 
 # Application definition
 
